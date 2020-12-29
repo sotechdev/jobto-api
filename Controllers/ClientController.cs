bd65e7d557ac763047031e6365b135ef91957f28
Osni Pezzini Junior <osni.pezzini@gmail.com>
2020-12-28 21:39:39 -0300

    Criado controller de clientes

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,33 @@
+﻿using JobTo.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JobTo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> GetClients()
+        {
+            return Ok(new List<People>
+            {
+                new People
+                {
+                    Id = 1,
+                    Name = "SOTech Sistemas",
+                    Document = "17.021.901/0001-62"
+                }
+            });
+        }
+    }
+}
