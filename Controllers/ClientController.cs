8b88084f8d636eaffce78dc3608bfc5da8395823
Osni Pezzini Junior <osni.pezzini@gmail.com>
2021-02-28 15:29:37 -0300

    Updated ClientController to use repository

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -1,7 +1,7 @@
-﻿using JobTo.Common.Models;
+﻿using JobTo.Common.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace JobTo.API.Controllers
@@ -11,23 +11,31 @@ namespace JobTo.API.Controllers
     public class ClientController : ControllerBase
     {
         readonly ILogger<ClientController> _logger;
-
-        public ClientController(ILogger<ClientController> logger)
+        readonly IClientRepository _repository;
+        public ClientController(ILogger<ClientController> logger, IClientRepository repository)
         {
             _logger = logger;
+            _repository = repository;
         }
 
+        /// <summary>
+        /// Metodo para retornar todos os clientes
+        /// </summary>
+        /// <returns>
+        /// Uma resposta com os dados dos clientes.
+        /// </returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response> 
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetClients()
         {
-            return Ok(new List<People>
-            {
-                new People
-                {
-                    Id = 1,
-                    Name = "SOTech Sistemas",
-                    Document = "17.021.901/0001-62"
-                }
-            });
+            var clients = await _repository.All();
+            _logger.LogInformation($"Retornando {clients.Count} cliente(s)");
+            return Ok(clients);
         }
     }
 }
